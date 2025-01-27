6a2d514f8dc1a79a578cbb3eb95efce25e3c9fe7
rishit_gupta <rishit_gupta@epam.com>
2025-01-27 09:39:21 +0530

    Added a New Game

--- src/main/java/com/rishit/learn_spring_framework/game/App.java
+++ src/main/java/com/rishit/learn_spring_framework/game/App.java
@@ -3,11 +3,9 @@ package com.rishit.learn_spring_framework.game;
 public class App {
     public static void main(String[] args) {
         var gameRunner = new GameRunner(
-                new Contra()
+                new PacMan()
         );
 
-        gameRunner.run();
-
-        gameRunner.getGame().gameMove("click");
+        gameRunner.getGame().gameMove("up");
     }
 }
---
+++ src/main/java/com/rishit/learn_spring_framework/game/PacMan.java
@@ -0,0 +1,14 @@
+package com.rishit.learn_spring_framework.game;
+
+public class PacMan implements GameConsole{
+    @Override
+    public void gameMove(String move) {
+        switch (move){
+            case "up" -> System.out.println("Go UP");
+            case "down" -> System.out.println("Go DOWN");
+            case "left" -> System.out.println("Go LEFT");
+            case "right" -> System.out.println("Go RIGHT");
+            default -> System.out.println("Enter valid move");
+        }
+    }
+}
