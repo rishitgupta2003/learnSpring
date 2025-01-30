31d92f3c290cdb53d2c4edf74622739091f31514
rishit_gupta <rishit_gupta@epam.com>
2025-01-30 14:51:48 +0530

    Initiated Spring

---
+++ src/main/java/com/rishit/h2_jpa_hibernate/H2JpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.rishit.h2_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2JpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2JpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishit/h2_jpa_hibernate/H2JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishit.h2_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
