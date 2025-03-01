46a7263492089f3aae4947aa3c0903935c47b46a
rishitgupta203 <rishitgupta203@gmail.com>
2025-03-01 10:56:46 +0530

    Implemented Logging in Employee Controller

--- src/main/java/com/rishit/spring_boot/controller/EmployeeController.java
+++ src/main/java/com/rishit/spring_boot/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.rishit.spring_boot.service.EmployeeService;
 import com.rishit.spring_boot.util.response_handlers.ApiException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 @AllArgsConstructor
 @RequestMapping("/api/employees")
 public class EmployeeController {
+    private final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
     private final EmployeeService employeeService;
 
     private EntityModel<EmployeeDTO> toEntityModel(EmployeeDTO employeeDTO){
@@ -33,12 +36,14 @@ public class EmployeeController {
     public ResponseEntity<List<EntityModel<EmployeeDTO>>> getAllEmployee(){
         List<EmployeeDTO> allEmployees = employeeService.getAllEmployees();
         List<EntityModel<EmployeeDTO>> list = allEmployees.stream().map(this::toEntityModel).toList();
+        logger.info("All Employees Fetched");
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EntityModel<EmployeeDTO>> getEmployeeById(@PathVariable Long id){
         EmployeeDTO employeeById = employeeService.getEmployeeById(id);
+        logger.info("Employee Fetched with ID -> {}" , id);
         return new ResponseEntity<>(toEntityModel(employeeById), HttpStatus.OK);
     }
 
@@ -46,12 +51,14 @@ public class EmployeeController {
     public ResponseEntity<List<EntityModel<EmployeeDTO>>> addAllEmployees(@RequestBody @Valid List<Employee> employees){
         List<EmployeeDTO> employeeDTOS = employeeService.addAllEmployees(employees);
         List<EntityModel<EmployeeDTO>> list = employeeDTOS.stream().map(this::toEntityModel).toList();
+        logger.info("All Employees Added Successfully");
         return new ResponseEntity<>(list, HttpStatus.CREATED);
     }
 
     @PostMapping("/")
     public ResponseEntity<EntityModel<EmployeeDTO>> addEmployee(@RequestBody @Valid Employee employee){
         EmployeeDTO employeeDTO = employeeService.createEmployee(employee);
+        logger.info("Employee Added Successfully");
         return new ResponseEntity<>(toEntityModel(employeeDTO), HttpStatus.CREATED);
     }
 }
